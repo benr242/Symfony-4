019e162e06386a3e41b07bcf1dd88e6bc31d20eb
Benjamin Rose <benr242@localhost.localdomain>
2019-04-13 18:00:07 -0400

    check

---
+++ src/DataFixtures/ProductFixture.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ProductFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $product = new Product();
+        $product->setName('Priceless widget!');
+        $product->setPrice(14.50);
+        $product->setDescription('Ok, I guess it *does* have a price');
+        $manager->persist($product);
+
+        $manager->flush();
+    }
+}
