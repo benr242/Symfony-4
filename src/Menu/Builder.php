e18c3b647a5de3c890ab47a8d6e3612a21c3a61c
Benjamin Rose <benr242@localhost.localdomain>
2019-05-04 14:50:44 -0400

    menu medium

--- src/Menu/Builder.php
+++ src/Menu/Builder.php
@@ -12,7 +12,33 @@ use Knp\Menu\FactoryInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-class Builder
+//https://symfony.com/doc/current/bundles/KnpMenuBundle/index.html
+class Builder implements ContainerAwareInterface
 {
+    use ContainerAwareTrait;
 
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+
+        $menu->addChild('Home', ['route' => 'welcome']);
+
+        // access services from the container!
+        $em = $this->container->get('doctrine')->getManager();
+
+        $menu->addChild('Latest Blog Post', [
+            'route' => 'blog_show',
+            'routeParameters' => ['id' => $blog->getId()]
+        ]);
+
+
+        // create another menu item
+        $menu->addChild('About Me', ['route' => 'about']);
+        // you can also add sub levels to your menus as follows
+        $menu['About Me']->addChild('Edit profile', ['route' => 'edit_profile']);
+
+        // ... add more children
+
+        return $menu;
+    }
 }
